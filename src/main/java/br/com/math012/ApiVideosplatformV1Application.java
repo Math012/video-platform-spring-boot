877931ee90216149f998a3321bf8bcb0253b8170
Math <math111.0811@gmail.com>
2023-08-26 14:17:34 -0300

    initial commit

---
+++ src/main/java/br/com/math012/ApiVideosplatformV1Application.java
@@ -0,0 +1,13 @@
+package br.com.math012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVideosplatformV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVideosplatformV1Application.class, args);
+	}
+
+}
