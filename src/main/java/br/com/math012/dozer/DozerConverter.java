9e4c54946b522dd9f47f7f8f454fc67e3b84410b
Math <math111.0811@gmail.com>
2023-09-01 15:11:24 -0300

    adicionando AccountVO

--- src/main/java/br/com/math012/dozer/DozerConverter.java
+++ src/main/java/br/com/math012/dozer/DozerConverter.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DozerConverter {
-
     private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
     public static <O, D> D parseObject(O origin, Class<D> destination){
