af215bf5dd89a7b5aa4a7e490c96d4fd551bf361
Math <math111.0811@gmail.com>
2023-09-04 13:22:50 -0300

    adicionando a classe CustomHandlerException

---
+++ src/main/java/br/com/math012/exceptions/CustomHandlerException.java
@@ -0,0 +1,12 @@
+package br.com.math012.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomHandlerException extends ResponseEntityExceptionHandler {
+
+}
