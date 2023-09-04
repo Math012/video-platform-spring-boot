c6abe2ed4e1954513975f021cd9ccf61e8c05af4
Math <math111.0811@gmail.com>
2023-09-04 13:45:28 -0300

    adicionando as exceptions ao CustomHandlerException

--- src/main/java/br/com/math012/exceptions/CustomHandlerException.java
+++ src/main/java/br/com/math012/exceptions/CustomHandlerException.java
@@ -1,12 +1,44 @@
 package br.com.math012.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 @RestController
 public class CustomHandlerException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<StructCustomException> handlerNotFoundException(Exception e, WebRequest request){
+        var exception = new StructCustomException(
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ExistingObjectException.class)
+    public ResponseEntity<StructCustomException> handlerExistingObjectException(Exception e, WebRequest request){
+        var exception = new StructCustomException(
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidTokenJwtException.class)
+    public ResponseEntity<StructCustomException> handlerInvalidTokenJwtException(Exception e, WebRequest request){
+        var exception = new StructCustomException(
+                new Date(),
+                e.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exception, HttpStatus.FORBIDDEN);
+    }
 
 }
