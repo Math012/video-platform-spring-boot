3a6d23f761d4edef8c7aa35fbaf5bc57245ea9c7
Math <math111.0811@gmail.com>
2023-09-05 15:28:22 -0300

    criando o AuthController e implementando o endpoint de login e cadastro

---
+++ src/main/java/br/com/math012/controller/AuthController.java
@@ -0,0 +1,43 @@
+package br.com.math012.controller;
+
+import br.com.math012.VO.AccountVO;
+import br.com.math012.VO.RegisterVO;
+import br.com.math012.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(value = "/login")
+    public ResponseEntity login(@RequestBody AccountVO account){
+        if (account == null
+                ||account.getUsername()==null
+                ||account.getUsername().isBlank()
+                ||account.getPassword().isBlank())
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid user");
+        var token = authService.singin(account);
+        if (token == null){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid token");
+        }
+        return token;
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<?> register(@RequestBody RegisterVO account){
+        authService.registerUser(account);
+        URI location = URI.create(String.format("/api/auth/register/%s",account.getUsername()));
+        return ResponseEntity.created(location).body("User: " + account.getUsername()+", registered!");
+    }
+}
