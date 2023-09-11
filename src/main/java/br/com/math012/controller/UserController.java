2e09cb6482c2b8832dc5d1fd740accaeb5b5cf85
Math <math111.0811@gmail.com>
2023-09-11 16:06:27 -0300

    adicionando a anotacao ApiResponse para os verbos http

--- src/main/java/br/com/math012/controller/UserController.java
+++ src/main/java/br/com/math012/controller/UserController.java
@@ -1,8 +1,14 @@
 package br.com.math012.controller;
 
+import br.com.math012.VO.VideoVO;
 import br.com.math012.models.UserModel;
 import br.com.math012.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +27,12 @@ public class UserController {
 
     @Operation(summary = "Return the all users")
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Sucess", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = VideoVO.class)))}),
+            @ApiResponse(responseCode = "401", description = "Enauthorized", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal Error", content = @Content),
+    })
     public List<UserModel> findAll(){
         return userService.findAll();
     }
