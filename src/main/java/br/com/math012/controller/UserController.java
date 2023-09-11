a3544926911b7b415e3276d8a09aabfdc6d84db7
Math <math111.0811@gmail.com>
2023-09-11 15:05:18 -0300

    adicionando as anotacoes do swagger ao UserController

--- src/main/java/br/com/math012/controller/UserController.java
+++ src/main/java/br/com/math012/controller/UserController.java
@@ -2,6 +2,8 @@ package br.com.math012.controller;
 
 import br.com.math012.models.UserModel;
 import br.com.math012.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name="Endpoint for users")
 @RestController
 @RequestMapping(value = "/api/user/v1")
 public class UserController {
@@ -16,6 +19,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Return the all users")
     @GetMapping
     public List<UserModel> findAll(){
         return userService.findAll();
